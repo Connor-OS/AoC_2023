5eab0b623da781155f10b5e0e9a1b73207936039
Connor O'Shaughnessy <connor.oshaughnessy@version1.com>
2023-12-05 16:36:07 +0000

    add solution template

--- day_4.py
+++ day_4.py
@@ -1,19 +1,27 @@
-TEST = False
+TEST = True
 in_file = "./resources/day_4_test.txt" if TEST else "./resources/day_4_input.txt"
 
 
-def question_1(lines):
+def file_lines():
+    with open(in_file) as file:
+        for line in file:
+            line = line.split(":")[1]
+            win_numbers, my_numbers = line.split("|")
+            win_numbers = win_numbers.strip().split()
+            my_numbers = my_numbers.strip().split()
+            yield win_numbers, my_numbers
+
+
+def file_length():
+    with open(in_file) as file:
+        return len([i for i in file])
+
+
+def question_1():
     """Answer to the first question of the day"""
-    lines = lines.splitlines()
     total = 0
-    for line in lines:
+    for win_numbers, my_numbers in file_lines():
         score = 0
-        line = line.split(":")[1]
-        win_numbers, my_numbers = line.split("|")
-
-        win_numbers = win_numbers.strip().split()
-        my_numbers = my_numbers.strip().split()
-
         for num in win_numbers:
             if num in my_numbers:
                 if score == 0:
@@ -26,20 +34,15 @@ def question_1(lines):
     return total
 
 
-def question_2(lines):
+def question_2():
     """Answer to the second question of the day"""
-    lines = lines.splitlines()
-    copies = [1] * len(lines)
+
+    copies = [1] * file_length()
     pos = 0
-    for line, copy in zip(lines, copies):
+    for line, copy in zip(file_lines(), copies):
+        win_numbers, my_numbers = line
 
         matches = 0
-        line = line.split(":")[1]
-        win_numbers, my_numbers = line.split("|")
-
-        win_numbers = win_numbers.strip().split()
-        my_numbers = my_numbers.strip().split()
-
         for num in win_numbers:
             if num in my_numbers:
                 matches += 1
@@ -47,15 +50,12 @@ def question_2(lines):
         for i in range(1, matches + 1):
             copies[pos + i] += 1 * copy
         pos += 1
-
     return sum(copies)
 
 
 if __name__ == '__main__':
-    with open(in_file) as data:
-        answer = question_1(data.read())
-        print(answer)
+    answer = question_1()
+    print(answer)
 
-    with open(in_file) as data:
-        answer = question_2(data.read())
-        print(answer)
+    answer = question_2()
+    print(answer)
---
+++ day_x.py
@@ -0,0 +1,34 @@
+TEST = True
+in_file = "./resources/day_4_test.txt" if TEST else "./resources/day_4_input.txt"
+
+
+def file_lines():
+    with open(in_file) as file:
+        for line in file:
+            """Custom iteration logic goes here"""
+            line = line.strip()
+            yield line
+
+
+def question_1():
+    """Answer to the first question of the day"""
+    answer = None
+    for line in file_lines():
+        print(line)
+
+    return answer
+
+
+def question_2():
+    """Answer to the second question of the day"""
+    answer = None
+
+    return answer
+
+
+if __name__ == '__main__':
+    answer_1 = question_1()
+    print(f"Question 1 answer is: {answer_1}")
+
+    # answer_2 = question_2()
+    # print(f"Question 2 answer is: {answer_1}")
