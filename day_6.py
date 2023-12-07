196e6c19dd7b0e174f509272341b6cec88f71f4f
Connor O'Shaughnessy <connor.oshaughnessy@version1.com>
2023-12-07 09:19:53 +0000

    day 6 and 7

---
+++ day_6.py
@@ -0,0 +1,88 @@
+TEST = False
+in_file = "./resources/day_6_test.txt" if TEST else "./resources/day_6_input.txt"
+
+A = 1 #mm3
+
+def file_lines():
+    with open(in_file) as file:
+        for line in file:
+            """Custom iteration logic goes here"""
+            line = line.strip()
+            line = line.split(":")[1]
+            line = line.split()
+            line = [int(i) for i in line]
+
+            yield line
+
+def file_lines2():
+    with open(in_file) as file:
+        for line in file:
+            """Custom iteration logic goes here"""
+            line = line.strip()
+            line = line.split(":")[1]
+            line = line.split()
+            line = "".join(line)
+            line = int(line)
+
+            yield line
+
+
+
+def question_1():
+    """Answer to the first question of the day"""
+    answer = None
+    a = []
+    for i in file_lines():
+        a.append(i)
+    time, dist = a
+
+    races = []
+    for t,d in [i for i in zip(time,dist)]:
+        print(t,d)
+        total = 0
+        for v in range(1, t-1):
+            if win := v*(t-v) > d:
+                print(v)
+                print(win)
+                total += 1
+        races.append(total)
+
+    print(races)
+    answer = races[0]
+
+    for i in races[1:]:
+        answer *= i
+    return answer
+
+
+def question_2():
+    """Answer to the second question of the day"""
+    answer = None
+    a = []
+    for i in file_lines2():
+        # print(i)
+        a.append(i)
+    time, dist = a
+
+    upper = int(time/2)
+
+    lower = 0
+
+    mid = int((upper + lower) / 2)
+    while((upper-lower) > 1):
+        mid = int((upper + lower) / 2)
+        if mid*(time-mid) > dist:
+            upper = mid
+        else:
+            lower = mid
+
+    print(mid)
+    print(time - 2*(mid-1)-1)
+
+
+if __name__ == '__main__':
+    # answer_1 = question_1()
+    # print(f"Question 1 answer is: {answer_1}")
+
+    answer_2 = question_2()
+    print(f"Question 2 answer is: {answer_2}")
---
+++ day_7.py
@@ -0,0 +1,138 @@
+TEST = False
+in_file = "./resources/day_7_test.txt" if TEST else "./resources/day_7_input.txt"
+
+face_cards = {"A": "14", "K": "13", "Q": "12", "J": "11", "T": "10"}
+face_cards_joker = {"A": "14", "K": "13", "Q": "12", "J": "1", "T": "10"}
+
+BEST_HAND = 6
+HAND_CONST = 1000000000
+
+from itertools import groupby
+
+def file_lines():
+    with open(in_file) as file:
+        for line in file:
+            """Custom iteration logic goes here"""
+            line = line.strip()
+            line = line.split()
+            yield line
+
+
+def question_1():
+    """Answer to the first question of the day"""
+    answer = 0
+    hands = [(hand, int(bid)) for hand, bid in file_lines()]
+
+    hands = sorted(hands, key=lambda x: calc_hand_power(x[0]))
+
+    for i, hand in enumerate(hands):
+        # print(hand[0], i+1)
+        answer += hand[1] * (i+1)
+
+    return answer
+
+
+def calc_hand_power(hand):
+    power = 0
+    # convert  face cards to numerical
+    hand = list(hand)
+
+    for f in face_cards:
+        hand = [card.replace(f, face_cards[f]) for card in hand]
+    # check type
+    sorted_hand = sorted(hand)
+    groups = []
+    for k, g in groupby(sorted_hand):
+        groups.append(list(g))
+
+    for group in groups:
+        if len(group) == 5:
+            # 5 of a kind
+            power += HAND_CONST * BEST_HAND
+        if len(group) == 4:
+            # 4 of a kind
+            power += HAND_CONST * (BEST_HAND - 1)
+        if len(group) == 3:
+            # 3 of a kind
+            power += HAND_CONST * (BEST_HAND - 3)
+        if len(group) == 2:
+            # pair
+            power += HAND_CONST * (BEST_HAND - 5)
+
+    # add individual power
+    for i, card in enumerate(hand):
+        power += (14**(6-i)) * int(card)
+
+    return power
+
+
+def question_2():
+    """Answer to the second question of the day"""
+    """Answer to the first question of the day"""
+    answer = 0
+    hands = [(hand, int(bid)) for hand, bid in file_lines()]
+
+    hands = sorted(hands, key=lambda x: calc_hand_power_jokers(x[0]))
+
+    powers = [calc_hand_power_jokers(h[0]) for h in hands]
+
+    for i, hand in enumerate(hands):
+        # print(hand[0], i+1)
+        answer += hand[1] * (i+1)
+
+    return answer
+
+
+def calc_hand_power_jokers(hand):
+    power = 0
+    # convert  face cards to numerical
+    hand = list(hand)
+
+    for f in face_cards:
+        hand = [card.replace(f, face_cards_joker[f]) for card in hand]
+    # check type
+    sorted_hand = sorted(hand)
+    groups = []
+    for k, g in groupby(sorted_hand):
+        g = list(g)
+        # remove joker group
+        if "1" in g:
+            continue
+        groups.append(g)
+    groups.append([])
+
+    #add jokers to the biggest other group
+    for card in hand:
+        if card == "1":
+            groups = sorted(groups, key=lambda x: 5-len(x))
+            groups[0].append("J")
+
+    for group in groups:
+        if len(group) == 5:
+            # 5 of a kind
+            power += HAND_CONST * BEST_HAND
+        if len(group) == 4:
+            # 4 of a kind
+            power += HAND_CONST * (BEST_HAND - 1)
+        if len(group) == 3:
+            # 3 of a kind
+            power += HAND_CONST * (BEST_HAND - 3)
+        if len(group) == 2:
+            # pair
+            power += HAND_CONST * (BEST_HAND - 5)
+
+    # add individual power
+    for i, card in enumerate(hand):
+        power += (14**(6-i)) * int(card)
+
+
+    return power
+
+
+
+if __name__ == '__main__':
+    answer_1 = question_1()
+    print(f"Question 1 answer is: {answer_1}")
+
+    answer_2 = question_2()
+    print(f"Question 2 answer is: {answer_2}")
